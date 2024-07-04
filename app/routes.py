346d4155e1e68ad136c7bf42f86adfbc6b4bf7e4
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-07-04 19:04:37 +0400

    ok

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,13 @@ posts = []
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        title = request.form.get('title')
-        content = request.form.get('content')
-        if title and content:
-            posts.append({'title': title, 'content': content})
+        name = request.form.get('name')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        age = request.form.get('age')
+
+        if name and city and hobby and age:
+            posts.append({'name': name, 'city': city, 'hobby': hobby, 'age': age})
             return redirect(url_for('index'))
-    return render_template('blog.html', posts=posts)
+
+    return render_template('index.html', posts=posts)
